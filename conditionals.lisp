08663c87ce269baa5b7d22b79743c02ee46a8333
Dominic Humphries <dominich@broadbean.com>
2015-08-25 17:10:09 +0100

    Conditionals!

---
+++ conditionals.lisp
@@ -0,0 +1,44 @@
+; The basic standards: if, if-else, when, unless, and a switch/case
+
+; If:
+(if '(1)
+  (princ "yes"))
+;> "yes"
+(if ()
+  (princ "yes"))
+;>
+
+; If-else:
+(if '(1)
+  (princ "yes")
+  (princ "no"))
+;> "yes"
+(if ()
+  (princ "yes")
+  (princ "no"))
+;> "no"
+
+; When:
+(when '(1)
+  (print "I do one thing")
+  (print "Then another!")
+  (print "'if' can only handle one thing")
+  (print "Unless you use progn, that is"))
+;> "I do one thing"
+;> "Then another!"
+;> "'if' can only handle one thing"
+;> "Unless you use progn, that is"
+
+; Unless is just a negated 'when'
+; Then there's cond - the switch/case
+(cond ((> 2 x) (princ "First"))
+      ((> 5 x) (princ "Second"))
+      (t (princ "Default")))
+; If x = 1
+;> "First"
+; if x = 3
+;> "Second"
+; if x = 10
+;> "Default"
+; There's no fall-through - you get the first match and then you're done.
+; Which IMHO is a far more sensible approach
