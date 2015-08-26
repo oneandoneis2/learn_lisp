1ee1735e89d022e97f8a7c9c38e93b430e16c04c
Dominic Humphries <dominich@broadbean.com>
2015-08-26 18:43:37 +0100

    Add case to conds

--- conditionals.lisp
+++ conditionals.lisp
@@ -1,4 +1,4 @@
-; The basic standards: if, if-else, when, unless, and a switch/case
+; The basic standards: if, if-else, when, unless, and two switch's
 
 ; If:
 (if '(1)
@@ -30,10 +30,15 @@
 ;> "Unless you use progn, that is"
 
 ; Unless is just a negated 'when'
-; Then there's cond - the switch/case
+; Then there's cond - the first switch
 (cond ((> 2 x) (princ "First"))
       ((> 5 x) (princ "Second"))
       (t (princ "Default")))
+; and case, the other switch
+(case x
+  ((1) (princ "First"))
+  ((3) (princ "Second"))
+  (otherwise (princ "Default")))
 ; If x = 1
 ;> "First"
 ; if x = 3
@@ -42,9 +47,11 @@
 ;> "Default"
 ; There's no fall-through - you get the first match and then you're done.
 ; Which IMHO is a far more sensible approach
+; Use case when always looking at the same var, cond for everything else
 
 ; There are also the "stealth" conditionals, which are helpful if you need short-circuiting
 ; "and" and "or"!
+; (note that 'if' also short-circuits, though)
 (and (print "one") (print "two"))
 ;> "one"
 ;> "two"
