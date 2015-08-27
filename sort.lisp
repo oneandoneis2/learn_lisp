d86be946461ad1c4395623c6a5358f5243e0659a
Dominic Humphries <dominich@broadbean.com>
2015-08-27 15:18:28 +0100

    Name corrections

--- sort.lisp
+++ sort.lisp
@@ -2,9 +2,9 @@
   (if lst
     (let* ((head (car lst))
            (tail (cdr lst))
-           (less (remove-if (lambda (x) (not (funcall fun x head))) tail))
-           (great (remove-if (lambda (x) (funcall fun x head)) tail)))
-      (append (mysort less fun) (list head) (mysort great fun)))
+           (left (remove-if (lambda (x) (not (funcall fun x head))) tail))
+           (right (remove-if (lambda (x) (funcall fun x head)) tail)))
+      (append (mysort left fun) (list head) (mysort right fun)))
   nil))
 
 (princ (mysort '(50 1 3 5 2 4 6 100) #'<))
