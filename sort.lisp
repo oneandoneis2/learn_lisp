c4b7deccccf7bc0cf5ab98836c996d235d23ab40
Dominic Humphries <dominich@broadbean.com>
2015-08-27 15:16:56 +0100

    Make mysort take a sorting function

--- sort.lisp
+++ sort.lisp
@@ -1,10 +1,10 @@
-(defun mysort (lst)
+(defun mysort (lst fun)
   (if lst
     (let* ((head (car lst))
            (tail (cdr lst))
-           (less (remove-if (lambda (x) (or (> x head) (eq head x))) tail))
-           (great (remove-if (lambda (x) (< x head)) tail)))
-      (append (mysort less) (list head) (mysort great)))
+           (less (remove-if (lambda (x) (not (funcall fun x head))) tail))
+           (great (remove-if (lambda (x) (funcall fun x head)) tail)))
+      (append (mysort less fun) (list head) (mysort great fun)))
   nil))
 
-(princ (mysort '(50 1 3 5 2 4 6 100)))
+(princ (mysort '(50 1 3 5 2 4 6 100) #'<))
