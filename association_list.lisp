88c5cac501af522794bf359c601da19f3624a3c9
Dominic Humphries <dominich@broadbean.com>
2015-09-09 15:29:17 +0100

    Add plist

--- association_list.lisp
+++ association_list.lisp
@@ -1,4 +1,4 @@
-; An association list, or hash
+; An association list, or alist
 (defparameter *foo* '((inquisition . "Nobody expects the spanish inquisition!")
                       (lumberjack . "I'm a lumberjack and I'm okay")
                       (brian . "The babe they call Brian")))
@@ -6,3 +6,13 @@
 ;> (LUMBERJACK . "I'm a lumberjack and I'm okay")
 (cdr (assoc 'lumberjack *foo*))
 ;> "I'm a lumberjack and I'm okay"
+
+; A property list, or plist
+(defparameter *bar* '(parrot "It's dead, that's what's wrong with it!"
+                      :gourd "How much do you want for it?"
+                      three "Three shall be the number thou shalt count"))
+(getf *bar* 'three)
+;> 2
+(getf *bar* :gourd)
+;> "How much do you want for it?"
+
