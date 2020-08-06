7a946ca07041853b0c09933b930c531717f97bfc
Dominic Humphries <dominich@broadbean.com>
2020-08-06 17:01:29 +0100

    DRY

--- pcl/test.lisp
+++ pcl/test.lisp
@@ -1,12 +1,14 @@
 (defun report-result (result form)
   (format t "~:[FAIL~;pass~] ... ~a~%" result form))
 
-(defmacro check (form)
-  `(report-result ,form ',form))
+(defmacro check (&body forms)
+  `(progn
+     ,@(loop for form in forms collect `(report-result ,form ',form))))
 
 (defun test-+ ()
-  (check (= (+ 1 2) 3))
-  (check (= (+ 1 2 3) 6))
-  (check (= (+ -1 -3) -5)))
+  (check
+    (= (+ 1 2) 3)
+    (= (+ 1 2 3) 6)
+    (= (+ -1 -3) -5)))
 
 (test-+)
