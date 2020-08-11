5838e5810f1051ae89726954f58ad559c9c33e17
Dominic Humphries <dominich@broadbean.com>
2020-08-11 16:32:13 +0100

    Update test to return an overall "pass/fail"

--- pcl/test.lisp
+++ pcl/test.lisp
@@ -1,8 +1,19 @@
+(defmacro wgs ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
 (defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
+  result)
+
+(defmacro combine-results (&body forms)
+  (wgs (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
 
 (defmacro check (&body forms)
-  `(progn
+  `(combine-results
      ,@(loop for form in forms collect `(report-result ,form ',form))))
 
 (defun test-+ ()
@@ -11,4 +22,4 @@
     (= (+ 1 2 3) 6)
     (= (+ -1 -3) -5)))
 
-(test-+)
+(princ (test-+))
